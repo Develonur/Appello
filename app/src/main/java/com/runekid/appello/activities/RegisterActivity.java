673b27bd2d1becafc1dd5994ad3c2077ee0ab0b2
Runekid <onur.bugdayci@student.ehb.be>
2018-05-08 23:35:07 +0200

    Login/Register/User - activities all work with basic user information

--- app/src/main/java/com/runekid/appello/activities/RegisterActivity.java
+++ app/src/main/java/com/runekid/appello/activities/RegisterActivity.java
@@ -1,8 +1,137 @@
 package com.runekid.appello.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
+
+import com.runekid.appello.R;
+import com.runekid.appello.helper.InputValidation;
+import com.runekid.appello.model.User;
+import com.runekid.appello.sql.DBHelper;
+
 /**
  * Created by Runekid on 08/05/2018.
  */
 
-public class RegisterActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final AppCompatActivity activity = RegisterActivity.this;
+
+    private NestedScrollView nestedScrollView;
+
+    private TextInputLayout textInputLayoutName;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputLayout textInputLayoutConfirmPassword;
+
+    private TextInputEditText textInputEditTextName;
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+    private TextInputEditText textInputEditTextConfirmPaswword;
+
+    private AppCompatButton appCompatButtonRegister;
+    private AppCompatTextView appCompatTextViewLoginLink;
+
+    private InputValidation inputValidation;
+    private DBHelper dbHelper;
+    private User user;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+
+        initViews();
+        initListeners();
+        initObjects();
+    }
+
+    private void initViews() {
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+
+        textInputLayoutName = (TextInputLayout) findViewById(R.id.textInputLayoutName);
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
+        textInputLayoutConfirmPassword = (TextInputLayout) findViewById(R.id.textInputLayoutConfirmPassword);
+
+        textInputEditTextName = (TextInputEditText) findViewById(R.id.textInputEditTextName);
+        textInputEditTextEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
+        textInputEditTextPassword = (TextInputEditText) findViewById(R.id.textInputEditTextPassword);
+        textInputEditTextConfirmPaswword = (TextInputEditText) findViewById(R.id.textInputEditTextConfirmPassword);
+
+        appCompatButtonRegister = (AppCompatButton) findViewById(R.id.appCompatButtonRegister);
+
+        appCompatTextViewLoginLink = (AppCompatTextView) findViewById(R.id.appCompatTextViewLoginLink);
+    }
+
+    private void initListeners() {
+        appCompatButtonRegister.setOnClickListener(this);
+        appCompatTextViewLoginLink.setOnClickListener(this);
+    }
+
+    private void initObjects() {
+        dbHelper = new DBHelper(activity);
+        inputValidation = new InputValidation(activity);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.appCompatButtonRegister:
+                postDataToSQLite();
+                break;
+            case R.id.appCompatTextViewLoginLink:
+                finish();
+                break;
+        }
+    }
+
+    private void postDataToSQLite() {
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextName,textInputLayoutName,getString(R.string.error_message_name))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextEmail,textInputLayoutEmail,getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextEmail(textInputEditTextEmail,textInputLayoutEmail,getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword,textInputLayoutPassword,getString(R.string.error_message_password))){
+            return;
+        }
+        if (!inputValidation.isInputEditTextMatches(textInputEditTextPassword,textInputEditTextConfirmPaswword,textInputLayoutConfirmPassword,getString(R.string.error_password_match))){
+            return;
+        }
+
+        if (!dbHelper.checkUser(textInputEditTextEmail.getText().toString().trim())) {
+            user = new User();
+            user.setName(textInputEditTextName.getText().toString().trim());
+            user.setEmail(textInputEditTextEmail.getText().toString().trim());
+            user.setPassword(textInputEditTextPassword.getText().toString().trim());
+
+            dbHelper.addUser(user);
+
+
+            Snackbar.make(nestedScrollView,getString(R.string.success_message),Snackbar.LENGTH_LONG).show();
+            emptyInputEditText();
+        } else {
+            Snackbar.make(nestedScrollView,getString(R.string.error_email_exists),Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void emptyInputEditText() {
+        textInputEditTextName.setText(null);
+        textInputEditTextEmail.setText(null);
+        textInputEditTextPassword.setText(null);
+        textInputEditTextConfirmPaswword.setText(null);
+    }
 }
--- app/src/main/java/com/runekid/appello/activities/UsersActivity.java
+++ app/src/main/java/com/runekid/appello/activities/UsersActivity.java
@@ -1,8 +1,29 @@
 package com.runekid.appello.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.runekid.appello.R;
+
 /**
  * Created by Runekid on 08/05/2018.
  */
 
-public class UsersActivity {
+public class UsersActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+
+        textView = (TextView) findViewById(R.id.text1);
+
+        String nameFromIntent = getIntent().getStringExtra("EMAIL");
+
+        textView.setText("Welcome " + nameFromIntent);
+    }
 }
